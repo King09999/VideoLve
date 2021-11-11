ce50cc53a4e4dfac12de71127d769133fef1d225
King09999 <90600929+King09999@users.noreply.github.com>
2021-11-12 01:04:13 +0530

    Update nopm.py

--- plugins/nopm.py
+++ plugins/nopm.py
@@ -43,7 +43,7 @@ async def nopm(client, message):
             OLD_PMS[message.chat.id] = {"msg":m.updates[1].message.id, "s":message.message_id}
         except BotInlineDisabled:
             print(f"[WARN] - Inline Mode for @{USERNAME} is not enabled. Enable from @Botfather to enable PM Permit !")
-            await message.reply_text(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@ZauteKm | @JosProjects 👑</b>")
+            await message.reply_text(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@Loltage | @Kolixsupport 👑</b>")
         except Exception as e:
             print(e)
             pass
