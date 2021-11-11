8c09046536376e5ed69dfb569a6c2fb7a264dca8
King09999 <90600929+King09999@users.noreply.github.com>
2021-11-12 00:57:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/ZauteKm/VideoPlayerBot.git /VideoPlayerBot
+git clone -b master https://github.com/King09999/VideoLve /VideoPlayerBot
 cd /VideoPlayerBot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
